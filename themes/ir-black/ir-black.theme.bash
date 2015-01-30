fa58078147064bd0fe3ca52cca5ca5ed9593416e
Jerry Xu <jerry@lyft.com>
2015-01-29 17:55:17 -0800

    Support virtualenv prompt

--- themes/ir-black/ir-black.theme.bash
+++ themes/ir-black/ir-black.theme.bash
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-prompt() {
+function virtualenv_prompt() {
+  if [ ! -z "$VIRTUAL_ENV" ]
+  then
+    local env=$(dirname $VIRTUAL_ENV)
+    echo " [${env##*/}]"
+  fi
+}
+
+function prompt() {
   return_value=$?;
   if [ $IS_REMOTE = 'true' ] ; then
-  	prompt="${purple}\w${reset_color}${cyan}$(__git_ps1)${reset_color}"
+  	prompt="${purple}\w${reset_color}${cyan}$(__git_ps1)${reset_color}${bold_blue}$(virtualenv_prompt)${reset_color}"
   else
-  	prompt="${green}\w${reset_color}${cyan}$(__git_ps1)${reset_color}"
+  	prompt="${green}\w${reset_color}${cyan}$(__git_ps1)${reset_color}${bold_blue}$(virtualenv_prompt)${reset_color}"
   fi
   if test $return_value -eq 0
   then
