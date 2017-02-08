4753d65f4cdf0d2c4d3100df265319204da04056
Jerry Xu <jerrysxu@gmail.com>
2017-02-07 22:17:56 -0800

    Add support for anaconda prompt

--- themes/ir-black/ir-black.theme.bash
+++ themes/ir-black/ir-black.theme.bash
@@ -8,12 +8,20 @@ function virtualenv_prompt() {
   fi
 }
 
+function conda_prompt() {
+  if [ ! -z "$CONDA_DEFAULT_ENV" ]
+  then
+    local env=$(dirname $CONDA_DEFAULT_ENV)
+    echo " [${CONDA_DEFAULT_ENV}]"
+  fi
+}
+
 function prompt() {
   return_value=$?;
   if [ $IS_REMOTE = 'true' ] ; then
-  	prompt="${purple}\w${reset_color}${cyan}$(__git_ps1)${reset_color}${bold_blue}$(virtualenv_prompt)${reset_color}"
+  	prompt="${purple}\w${reset_color}${cyan}$(__git_ps1)${reset_color}${bold_blue}$(virtualenv_prompt)${bold_purple}$(conda_prompt)${reset_color}"
   else
-  	prompt="${green}\w${reset_color}${cyan}$(__git_ps1)${reset_color}${bold_blue}$(virtualenv_prompt)${reset_color}"
+  	prompt="${green}\w${reset_color}${cyan}$(__git_ps1)${reset_color}${bold_blue}$(virtualenv_prompt)${bold_purple}$(conda_prompt)${reset_color}"
   fi
   if test $return_value -eq 0
   then
